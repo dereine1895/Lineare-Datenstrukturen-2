75e80819c4596a8949e535e415f8eddfad9bda64
Tom <tom@mvjt.de>
2025-01-24 10:40:17 +0100

    Node anfang

---
+++ src/Node.java
@@ -0,0 +1,24 @@
+public class Node<E> {
+
+    private E content;
+
+    public Node(E pContent) {
+    }
+
+    public E getContent() {
+    }
+
+    public setContent (E pContent) {
+    }
+
+    public Node getNextNode(){
+    }
+
+    public setNextNode(Node nextNode) {
+
+    }
+
+
+
+
+}
