2a486125739789381193c6e122455c7e97ec01ab
dereine1895 <tom@mvjt.de>
2025-01-28 16:41:01 +0100

    knotenpunkt fertig

--- src/Node.java
+++ src/Node.java
@@ -1,24 +1,31 @@
 public class Node<E> {
 
-    private E content;
+    private E content;  // Der Inhalt des Knotens
+    private Node<E> nextNode;  // Verweis auf den nächsten Knoten
 
+    // Konstruktor, um den Inhalt des Knotens zu setzen
     public Node(E pContent) {
+        this.content = pContent;
+        this.nextNode = null;  // Der nächste Knoten ist zu Beginn null
     }
 
+    // Getter für den Inhalt des Knotens
     public E getContent() {
+        return content;
     }
 
-    public setContent (E pContent) {
+    // Setter für den Inhalt des Knotens
+    public void setContent(E pContent) {
+        this.content = pContent;
     }
 
-    public Node getNextNode(){
+    // Getter für den nächsten Knoten
+    public Node<E> getNextNode() {
+        return nextNode;
     }
 
-    public setNextNode(Node nextNode) {
-
+    // Setter für den nächsten Knoten
+    public void setNextNode(Node<E> nextNode) {
+        this.nextNode = nextNode;
     }
-
-
-
-
 }
