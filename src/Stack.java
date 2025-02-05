1f2c529e803ac5ff72efdfecfc436b7f28c48b8d
Tom <tom@mvjt.de>
2025-02-05 10:36:56 +0100

    stack anfang

--- src/Stack.java
+++ src/Stack.java
@@ -1,18 +1,34 @@
 public class Stack <E> {
+    private NodeStack<E> top;
+
+public Stack(){
+    top =null;
+}
 
 public E getContent(){
-    return content;
-}
-public void add(E content){
-    if(content != null){
-        conten
+    if(top == null) {
+        System.out.println("Stack ist leer!");
+        return null;
     }
+    return top.content;
+}
+
+public void add(E content){
+    NodeStack<E> newNode = new NodeStack<> (content);
+    newNode.next = top;
+    top = newNode;
+
+
 }
 public void remove(){
-
+   if (top == null) {
+    System.out.println("Stack ist leer");
+   } else {
+    top = top.next;
+   }
 }
 public boolean isEmpty() {
-
-}
+    return top == null;
+    }
 
 }
